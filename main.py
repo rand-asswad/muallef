0f6e14ae96a0cd1a9528ca9eeb324417cf42f4a4
Rand Asswad <asswad.rand@gmail.com>
2018-05-16 21:34:23 +0200

    Report sent to Fortier

--- main.py
+++ main.py
@@ -10,12 +10,21 @@ inputFile = 'sounds/reiding-cut.wav'
 
 p = Part(inputFile)
 
-s = p.scale()
-print(s)
+#s = p.scale()
+#print(s)
 
+#play_wav(inputFile)
+
+p.plot_pitch(MIDI=True)
 wav_time = np.arange(p.file_data.size) / float(p.sample_rate)
+envelope = p.find_envelope()
 
-p.plot_pitch()
+from scipy.signal import argrelmin
+local_mins = argrelmin(envelope)
+t = [wav_time[i] for i in local_mins]
+a = [p.notes[i].midi for i in local_mins]
+plt.plot(t, a, "bo")
+
 
 # time = []
 # pitch = []
@@ -25,8 +34,10 @@ p.plot_pitch()
 # time = np.array(time)
 # pitch = np.array(pitch)
 
-# plt.plot(wav_time, p.file_data)
-# plt.plot(time, pitch, "ro")
-# plt.xlabel("Time (s)")
+# plt.plot(wav_time, p.file_data, label='signal')
+# plt.plot(wav_time, envelope, label='envelope')
+
+#plt.plot(time, pitch, "ro")
+plt.xlabel("Time (s)")
 # plt.ylabel("Freq (Hz)")
-# plt.show()
+plt.show()
