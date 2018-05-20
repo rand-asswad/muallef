5ae10c66b614674a6becb1d151d3162e2791d57a
Rand Asswad <asswad.rand@gmail.com>
2018-05-20 23:17:21 +0200

    Created audio module for reading audio files (not only wav)

--- muallef/audio.py
+++ muallef/audio.py
@@ -2,7 +2,7 @@ from os import path, remove
 from tempfile import mkstemp
 from scipy.io import wavfile
 import numpy as np
-from pydub import AudioSegment as seg
+from pydub import AudioSegment
 
 
 class source(object):
@@ -19,13 +19,21 @@ class source(object):
     def get_time(self, n_samples=None):
         if not n_samples:
             n_samples = self.size
-        samples = np.arange(start=0, stop=n_samples, dtype=float)
-        return samples / float(self.sampleRate)
+        #return np.arange(n_samples) / float(self.sampleRate)
+        n_ratio = self.duration / float(n_samples)
+        return np.arange(n_samples) * n_ratio
+
+    def cut(self, start=None, stop=None):
+        start_f = int(start * self.sampleRate) if start else 0
+        stop_f = int(stop * self.sampleRate) if stop else self.size
+        self.signal = self.signal[start_f:stop_f]
+        self.size = self.signal.size
+        self.duration = self.size / float(self.sampleRate)
 
 
 def read_file(file_path):
     file, ext = path.splitext(file_path)
-    sound = seg.from_file(file_path, ext[1:])
+    sound = AudioSegment.from_file(file_path, ext[1:])
     if ext == ".wav":
         tmp = None
         wav = file_path
